9912350afb7d2849e69b1f5631a7b0536b865bbd
Anastasiya <sulimova.a.m@yandex.ru>
2024-04-01 13:04:44 +0300

    refactoring index.html and index.js: add template and data.js

---
+++ src/data.js
@@ -0,0 +1,99 @@
+export const data = [
+    {
+        date: '6 апреля, 13:00',
+        name: 'МК Травяной сбор',
+        price: '1000 руб',
+        link: './trava.png',
+
+    },
+    {
+        date: '6 апреля, 17:00',
+        name: 'МК роспись свечей-спиралек и создание подсвечников из глины',
+        price: '2900 руб',
+        link: './candles.png',
+    },
+    {
+        date: '7 апреля, 15:00',
+        name: 'МК по тейпированию',
+        price: 'бесплатно',
+        link: 'https://i.pinimg.com/564x/61/1d/95/611d955b2a2c146263cfdd68caa8392f.jpg',
+    },
+    {
+        date: '7 апреля, 18:00',
+        name: 'МК варим мыло со скрабом',
+        price: '1500 руб',
+        link: './milo.png',
+    },
+    {
+        date: '13 апреля, 14:00',
+        name: 'МК флорариум: часть 1, паяем каркас',
+        price: '2500 руб',
+        link: 'https://i.pinimg.com/564x/60/24/f8/6024f86d1cf31219c227c865378cc293.jpg',
+    },
+    {
+        date: '13 апреля, 18:00',
+        name: 'МК рисуем акварелью',
+        price: '1900 руб',
+        link: 'https://i.pinimg.com/564x/f4/c1/bc/f4c1bc0c8b4e355c614ed64ad19c8f4d.jpg',
+    },
+    {
+        date: '14 апреля, 14:00',
+        name: 'МК флорариум: часть 2, составляем композицию',
+        price: '1300 руб',
+        link: 'https://i.pinimg.com/564x/60/24/f8/6024f86d1cf31219c227c865378cc293.jpg',
+    },
+    {
+        date: '14 апреля, 16:00',
+        name: 'МК бомбочки для ванны',
+        price: '1900 руб',
+        link: 'https://i.pinimg.com/564x/21/41/b9/2141b9756758f30f1c9fa6349e1812a1.jpg',
+    },
+    {
+        date: '14 апреля, 19:00',
+        name: 'МК украшения из эпоксидной смолы',
+        price: '2900 руб',
+        link: 'https://i.pinimg.com/564x/39/60/58/3960587c29700f347e7685d3e60f20fd.jpg',
+    },
+    {
+        date: '20 апреля, 14:00',
+        name: 'МК по свечеварению',
+        price: '1500 руб',
+        link: './svechi.png',
+    },
+    {
+        date: '20 апреля, 16:00',
+        name: 'Книжный клуб: "Кофемолка", Идов М.',
+        price: 'бесплатно',
+        link: 'https://img3.labirint.ru/rc/bc2569cd787031a909a3506f3e3c27f2/363x561q80/books22/215195/cover.jpg?1280394613',
+    },
+    {
+        date: '20 апреля, 18:00',
+        name: 'Киновечер: Сумерки',
+        price: 'бесплатно',
+        link: 'https://i.pinimg.com/564x/e1/03/82/e10382ea1e7f1fac10796bc6e1248252.jpg',
+    },
+    {
+        date: '21 апреля, 14:00',
+        name: 'МК эпоксидные подносы из дерева',
+        price: '3900 руб',
+        link: './podnosi.png',
+    },
+    {
+        date: '21 апреля, 18:00',
+        name: 'Public talk: современная мода',
+        price: 'donation',
+        link: 'https://i.pinimg.com/564x/65/b4/61/65b461465dca4aade6dc09516ab482b6.jpg',
+    },
+    {
+        date: '27 апреля, 12:00',
+        name: 'Своп: приносите свои вещи, забирайте чужие',
+        price: 'бесплатно',
+        link: 'https://i.pinimg.com/564x/b2/a4/c9/b2a4c9e1560e2182ce421edb23105a22.jpg',
+    },
+    {
+        date: '28 апреля, 14:00',
+        name: 'МК керамика',
+        price: '2300 руб',
+        link: 'https://i.pinimg.com/564x/bc/b1/ba/bcb1ba6f85672e3c782745a970db487b.jpg',
+    }
+];
--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './index.css';
+import { data } from './data';
+
+window.addEventListener('DOMContentLoaded', function () {
+    const gridElements = document.querySelector('.elements-grid');
+
+    function createCard(item) {
+        const gridTemplate = document.querySelector('#template-grid').content;
+        const gridElement = gridTemplate.querySelector('.elements-grid__item').cloneNode(true);
+        const gridPhoto = gridElement.querySelector('.elements-grid__img');
+    
+        gridPhoto.src = item.link;
+        gridPhoto.setAttribute('alt', item.name);
+        gridElement.querySelector('.elements-grid__name').textContent = item.name;
+        gridElement.querySelector('.elements-grid__date').textContent = item.date;
+        gridElement.querySelector('.elements-grid__price').textContent = item.price;
+    
+        return gridElement;
+    }
+
+    function insertCard(parent, date, name, price, link) {
+        const card = createCard({date, name, price, link});
+        parent.prepend(card);
+    }
+
+    data.reverse().forEach((item) => {
+        insertCard(gridElements, item.date, item.name, item.price, item.link)
+    });
+})
+
+
+
+
