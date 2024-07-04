24ca1ff4a4eb6cc9f6ded954329aadf1b97a0b05
Anastasiya Sulimova <breadkvass@gmail.com>
2024-07-04 19:43:32 +0300

    upd event assets & data

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: 'main_[hash].js',
     publicPath: '',
   },
   mode: 'development',
