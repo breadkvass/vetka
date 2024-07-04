5e45ca36e0f2fba7b7e37831b5317166d1f25876
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2024-07-04 19:59:31 +0300

    upd data july

--- src/data.js
+++ src/data.js
@@ -1,44 +1,8 @@
 export const data = [
     {
-        date: '8 июня, 17:00',
-        name: 'Киновечер',
-        price: 'бесплатно',
-        link: 'https://i.pinimg.com/originals/58/e9/65/58e96571acce841096995ad1e5d02cb6.jpg',
-    },
-    {
-        date: '15 июня, 14:00',
-        name: 'МК: Зеркало в пенной раме',
-        price: '',
-        link: 'https://i.pinimg.com/originals/5a/9c/48/5a9c48247caed139baae828025ea456e.jpg',
-    },
-    {
-        date: '16 июня, 17:00',
-        name: 'Киновечер',
-        price: 'бесплатно',
-        link: 'https://i.pinimg.com/originals/58/e9/65/58e96571acce841096995ad1e5d02cb6.jpg',
-    },
-    {
-        date: '22 июня, 16:00',
-        name: 'Книжный клуб: "Сиддхарта"',
-        price: '',
-        link: 'https://i.pinimg.com/564x/53/75/4a/53754a9f687452e00c09166af13f2148.jpg',
-    },
-    {
-        date: '23 июня, 18:00',
-        name: 'МК: вязание сумочки с авторской биркой',
-        price: '',
-        link: 'https://i.pinimg.com/736x/af/03/25/af032577e7668f782c39e12ecaadd6ef.jpg',
-    },
-    {
-        date: '29 июня, 14:00',
-        name: 'МК: керамика',
-        price: '',
-        link: 'https://i.pinimg.com/564x/c3/bf/39/c3bf3951a8e8e2dec0add19a05bfd498.jpg',
-    },
-    {
-        date: '30 июня, 19:00',
-        name: 'МК: вязание сумочки под бутылку',
-        price: '',
-        link: 'https://i.pinimg.com/originals/0d/05/19/0d051921f4f859f0e10a117b99566647.jpg',
+        date: '27 июля, 12:00',
+        name: `English Speaking Club: обсуждаем фильм Ноттинг Хилл`,
+        price: '1000 руб',
+        link: 'https://i.pinimg.com/originals/d2/e8/0b/d2e80b27bc555be4710315d6aaf76917.jpg',
     }
 ]
