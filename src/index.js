a5375788f7e135ccf57d5ecbd4add7ba34e3d2da
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2024-09-13 13:44:36 +0300

    upd with commenting events list

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 import './index.css';
-import { data } from './data';
+// import { data } from './data';
 
-window.addEventListener('DOMContentLoaded', function () {
-    const gridElements = document.querySelector('.elements-grid');
+// window.addEventListener('DOMContentLoaded', function () {
+//     const gridElements = document.querySelector('.elements-grid');
 
-    function createCard(item) {
-        const gridTemplate = document.querySelector('#template-grid').content;
-        const gridElement = gridTemplate.querySelector('.elements-grid__item').cloneNode(true);
-        const gridPhoto = gridElement.querySelector('.elements-grid__img');
+//     function createCard(item) {
+//         const gridTemplate = document.querySelector('#template-grid').content;
+//         const gridElement = gridTemplate.querySelector('.elements-grid__item').cloneNode(true);
+//         const gridPhoto = gridElement.querySelector('.elements-grid__img');
     
-        gridPhoto.src = item.link;
-        gridPhoto.setAttribute('alt', item.name);
-        gridElement.querySelector('.elements-grid__name').textContent = item.name;
-        gridElement.querySelector('.elements-grid__date').textContent = item.date;
-        gridElement.querySelector('.elements-grid__price').textContent = item.price;
+//         gridPhoto.src = item.link;
+//         gridPhoto.setAttribute('alt', item.name);
+//         gridElement.querySelector('.elements-grid__name').textContent = item.name;
+//         gridElement.querySelector('.elements-grid__date').textContent = item.date;
+//         gridElement.querySelector('.elements-grid__price').textContent = item.price;
     
-        return gridElement;
-    }
+//         return gridElement;
+//     }
 
-    function insertCard(parent, date, name, price, link) {
-        const card = createCard({date, name, price, link});
-        parent.prepend(card);
-    }
+//     function insertCard(parent, date, name, price, link) {
+//         const card = createCard({date, name, price, link});
+//         parent.prepend(card);
+//     }
 
-    data.reverse().forEach((item) => {
-        insertCard(gridElements, item.date, item.name, item.price, item.link)
-    });
-})
+//     data.reverse().forEach((item) => {
+//         insertCard(gridElements, item.date, item.name, item.price, item.link)
+//     });
+// })
 
 
 
